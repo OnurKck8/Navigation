9be898573fcfe6bf2ea642313f22cc74dde7a63c
KckOnur <124556360+OnurKck8@users.noreply.github.com>
2023-03-17 10:29:25 +0300

    FinishedProject
    A simple project to learn navigation

---
+++ Navigation/Assets/Scripts/navmeshscript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navmeshscript : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject,10f);
+    }
+}
